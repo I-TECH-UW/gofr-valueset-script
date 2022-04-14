dd36b5f3eb5a43fae84ed9892f3affc8e44ffda8
mozzy11 <mozzymutesa@gmail.com>
2022-04-14 23:52:56 +0300

    add scripts to load resources to gofr

---
+++ scripts/load-resources.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+baseurl=$1;
+username=$2;
+password=$3;
+token=`curl -XPOST -H 'Content-Type: application/json' -d '{"username": "'"$username"'", "password": "'"$password"'"}' ${baseurl}/auth/token | jq -r '.access_token'`
+
+curl -i -H "Content-type: application/json" -H "Authorization: Bearer ${token}" -X PUT ${baseurl}/fhir/DEFAULT/ValueSet/gofr-location-type-valueset -d @./resources/gofr-location-type-valueset.json
+
+curl -i -H "Content-type: application/json" -H "Authorization: Bearer ${token}" -X PUT ${baseurl}/fhir/DEFAULT/ValueSet/gofr-location-type-valueset -d @./resources/openelis-code-system.json
